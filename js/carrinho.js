fc67e12858a67f68b9ee38876d57a6da740d6637
Miguel Rinaldo <miguel.r.nascimento9@aluno.senai.br>
2026-07-14 19:04:59 -0300

    adicionei um js e mudei o meu html carrinho uma vez

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,32 +1,22 @@
 //CRIANDO O ARRAY DE ITENS DO CARRINHO
-const itensCarrinho = []
+const itensCarrinho = JSON.parse(localStorage.getItem('itensSessao')) || []
 
-//FUNÇÃO PARA ADICIONAR O ITEM NO ARRAY
-const addItem = (objItem)=>{
+//FUNÇÃO PARA ADCIONAR O ITEM NO ARRAY
+const addItem = (objItem) => {
     itensCarrinho.push(objItem)
 
- localStorage.setItem('itensSessao', itensCarrinho)
+    localStorage.setItem('itensSessao', itensCarrinho)
+    localStorage.setItem('itensSessao', JSON.stringify(itensCarrinho))
 }
 
 //LISTAR ITENS DO CARRINHO
 const listItens = () => {
-    const itensSelecionados = JSON.stringify(localStorage.getItem('itensSessao'))
+    
+   const itensSelecionados = JSON.parse(localStorage.getItem('itensSessao')) || []
 
-    return itensCarrinho
+    return itensSelecionados
 }
 
-//MONTANDO A TELA CARRINHO
-const montaTelaCarrinho = () =>{
-    const sectionItensCarrinho = document.querySelector('#itens-carrinho')
 
-    listItens().forEach((elem, i) =>{  
-    const sectionItem = document.createElement('section')
-    sectionItem.setAttribute('class', 'item')
-    sectionItem.innerHTML = `<img scr='${elem.caminho_da_imagem}' alt=${elem.descriacao_produto}/> <p class='descricao'>${elem.descriacao_produto}</p> <p class='vlr-unitario'>${elem.valor_unitario}</p> <imput type="number" name='quant${i}' id='quant${i}' class="input-item" value=${1}> <p calss="tot-item">${elem.valor_unitario * 1}</p> <img scr="../imagens/icones/remover.png" alt"" class="img-remover">`
-  sectionItensCarrinho.appendChild(sectionItem)
 
-})
-
-}
-
-export{addItem}
+export { addItem, listItens }
